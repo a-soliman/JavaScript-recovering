b53aa090606958a509374f3f5d53231730b86c76
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-12-26 13:37:47 -0800

    introduction to JS

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// === Introduction === 01
+
+// getName()
+
+
+// function getName(yourName) {
+
+// 	var yourName = prompt("What is your name?");
+
+// 	if (yourName != null && yourName.length > 3) {
+// 		document.getElementById("sayHello").innerHTML = "Hello " + yourName
+// 	} else {
+// 		alert("Please enter a name?")
+// 		getName()
+// 	}
+
+// }
+// =======================================================================
